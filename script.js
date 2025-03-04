6678e9b0c4d7cf932efd26decdaba18c9701aef8
糯竹 <87886224+Nuozhu2333@users.noreply.github.com>
2025-03-04 16:58:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,14 +14,7 @@ function close(){
 
 function showTooltip(event, content) {
   const tooltip = document.getElementById('tooltip');
-  const tooltipContent = document.getElementById('tooltip-content');
-  
   tooltipContent.innerHTML = content; 
-  
-  const rect = event.target.getBoundingClientRect();
-  
-  tooltip.style.left = rect.left + "px";
-  tooltip.style.top = rect.bottom + 10 + "px"; 
   tooltip.style.display = 'block';
 }
 
