63bc038285c94553c2b9c3cdda4b7692966f2cce
糯竹 <87886224+Nuozhu2333@users.noreply.github.com>
2025-03-04 16:40:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function closeNav() {
 function close(){
   window.close()
 }
+
+function showTooltip(event, content) {
+  const tooltip = document.getElementById('tooltip');
+  const tooltipContent = document.getElementById('tooltip-content');
+
+  tooltipContent.innerHTML = content; 
+
+  const rect = event.target.getBoundingClientRect();
+
+  tooltip.style.left = rect.left + "px";
+  tooltip.style.top = rect.bottom + 10 + "px";
+  tooltip.style.display = 'block';
+}
+
+function closeTooltip() {
+  const tooltip = document.getElementById('tooltip');
+  tooltip.style.display = 'none';
+}
