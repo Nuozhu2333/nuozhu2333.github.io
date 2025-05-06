a9cf5aa1d196651b5f18bfbb62b35a0194ef9abc
糯竹 <87886224+Nuozhu2333@users.noreply.github.com>
2025-05-06 23:48:57 +0800

    Add files via upload

---
+++ games/ESO/Guide-PWA/service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-pwa-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/Guide.html',
+        '/manifest.json',
+        '/logo.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
